dcfef9996f67b47d5021476538da63d8d5cf6107
Pol Milian <polcius@gmail.com>
2019-07-03 19:26:58 +1000

    casing bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import Intro from '../components/Intro';
-import SEO from '../components/SEO';
+import SEO from '../components/seo';
 import BodyText from '../components/BodyText';
 
 const IndexPage = () => (
