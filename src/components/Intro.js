168863f897321ef1a501f85eb61302e8f9d96f01
Pol Milian <polcius@gmail.com>
2019-07-07 15:06:59 +1000

    trying out styled system

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
+import {
+  space,
+  color,
+  fontSize,
+  width,
+  fontWeight,
+  lineHeight,
+} from 'styled-system';
 
 const IntroWrapper = styled.section`
   font-family: 'Ubuntu', sans-serif;
-  font-size: 36px;
-  color: ${props => props.theme.darkBlack};
   span {
-    color: ${props => props.theme.primary};
+    color: ${props => props.theme.colors.primary};
   }
   margin-bottom: 2rem;
+  ${color}
+  ${fontSize}
 `;
 
-
-
 const Intro = () => (
-  <IntroWrapper>
-    Hi, I’m <span>Pol.</span> A Full-Stack Web/UI Engineer.{' '}
+  <IntroWrapper color="darkBlack" fontSize={[2, 3, 4]}>
+    Hi, I’m <span>Pol.</span> A Full-Stack Web/UI Engineer from Barcelona.{' '}
     <span>Nice to meet you!</span>
-    
   </IntroWrapper>
 );
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,16 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { useSpring, animated } from 'react-spring';
-import { Normalize } from 'styled-normalize';
+import {
+  space,
+  color,
+  fontSize,
+  width,
+  fontWeight,
+  lineHeight,
+} from 'styled-system';
+
+// TODO: https://www.getwelly.com/ animacions text, underline https://www.nadiaux.design/
 
 const theme = {
-  darkBlack: '#1B1B1B',
-  lightBlack: '#3C3C3C',
-  primary: '#29D396',
+  fontSizes: [16, 24, 36, 48, 64, 96, 128],
+  space: [
+    // margin and padding
+    0,
+    4,
+    8,
+    16,
+    32,
+    48,
+    64,
+    128,
+    256,
+  ],
+  colors: {
+    darkBlack: '#1B1B1B',
+    lightBlack: '#3C3C3C',
+    primary: '#29D396',
+  },
 };
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Ubuntu|Vollkorn&display=swap');
+
+  * { 
+    box-sizing: border-box; 
+  }
 
   body {
     font-family: 'Vollkorn', serif;
@@ -37,7 +65,6 @@ const Layout = ({ children }) => {
   });
   return (
     <>
-      <Normalize />
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Main style={animation}>{children}</Main>
