0727b91808c9e9835cf66f8825cfa1a4772c6e3c
Pol Milian <polcius@gmail.com>
2020-07-01 16:33:48 +1000

    updates, removed twitter

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,14 +1,10 @@
 import React from 'react';
 // Icon files
-import TwitterIcon from '../images/svg/twitter-icon.svg';
 import GitHubIcon from '../images/svg/github-icon.svg';
 import LinkedInIcon from '../images/svg/linkedin-icon.svg';
 
 const SocialIcons = () => (
   <section className="flex flex-col items-center md:flex-row justify-center text-center mt-8 mb-16">
-    <a href="https://twitter.com/E_L_Pol" rel="noopener noreferrer">
-      <TwitterIcon className="px-2" width="96px" height="96px" />
-    </a>
     <a href="https://github.com/Polcius" rel="noopener noreferrer">
       <GitHubIcon className="px-2" width="96px" height="96px" />
     </a>
